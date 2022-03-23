de919646cd70d09c562af98d13aaa9e473f3fa88
mlane52 <mlane52@students.kennesaw.edu>
2022-03-23 18:35:20 -0400

    FlaskDemoProjectMatthewLane

--- 6 Web Page with Flask/basic page/script1.py
+++ 6 Web Page with Flask/basic page/script1.py
@@ -16,5 +16,9 @@ def home():
 def about():
     return render_template("about.html")
 
+@app.route('/matthew/')
+def Matthew():
+    return render_template("matthew.html")
+
 if __name__=="__main__":
     app.run(debug=True)
